5b11d2630f399c6510527e08547fad1f06935ad3
monica.pigrau <monibixv2@gmail.com>
2025-06-04 23:55:59 +0200

    SwapApp

--- test/SwapApp.t.sol
+++ test/SwapApp.t.sol
@@ -7,6 +7,9 @@ import "../src/SwapApp.sol";
 contract SwapAppTest is Test {
     SwapApp app;
     address uniswapV2SwappRouterAddress = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
+    address user = 0xF977814e90dA44bFA03b6295A0616a897441aceC; // Address with USDT in Arbitrum mainnet
+    address USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9; // USDT address in Arbitrum mainnet
+    address DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1; // DAI address in Arbitrum mainnet
 
     function setUp() public {
         app = new SwapApp(uniswapV2SwappRouterAddress);
@@ -16,5 +19,29 @@ contract SwapAppTest is Test {
     function testHasBeenDeployedCorrectly() public view {
         assert(app.V2Router02Address() == uniswapV2SwappRouterAddress);
     }
+
+    function testSwapTokensCorrectly() public {
+        vm.startPrank(user);
+        uint256 amountIn = 5 * 1e6; 
+        IERC20(USDT).approve(address(app), amountIn); 
+
+        uint256 amountOutMin = 4 * 1e18;
+        uint256 deadline = 1747815058 + 1000000000; 
+        address[] memory path = new address[](2);
+        path[0] = USDT;
+        path[1] = DAI;
+
+        uint256 usdtBalanceBefore = IERC20(USDT).balanceOf(user);
+        uint256 daiBalanceBefore = IERC20(DAI).balanceOf(user);
+        app.swapTokens(amountIn, amountOutMin, path, deadline);
+        uint256 usdtBalanceAfter = IERC20(USDT).balanceOf(user);
+        uint256 daiBalanceAfter = IERC20(DAI).balanceOf(user);
+
+        assert(usdtBalanceAfter == usdtBalanceBefore - amountIn);
+        assert(daiBalanceAfter > daiBalanceBefore); 
+
+        vm.stopPrank();
+    }
+
 
 }
